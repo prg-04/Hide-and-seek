e29595bde9c9660a88358db3b8a572fb6858f991
prg-04 <karanjaevans95@gmail.com>
2022-09-17 23:49:28 +0300

    initialize hide and seek

---
+++ script.js
@@ -0,0 +1,15 @@
+let menuIcon = documlet menuIcon = document.querySelector(".fa-bars-staggered");
+let sidebar = document.querySelector(".sidebar");
+let laContainer = document.querySelector(".laContainer");
+
+menuIcon.onclick = function () {
+  sidebar.classList.toggle("small-sidebar");
+  laContainer.classList.toggle("large-container");
+};ent.querySelector(".fa-bars-staggered");
+let sidebar = document.querySelector(".sidebar");
+let laContainer = document.querySelector(".laContainer");
+
+menuIcon.onclick = function () {
+  sidebar.classList.toggle("small-sidebar");
+  laContainer.classList.toggle("large-container");
+};
